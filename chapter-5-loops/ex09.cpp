667af0fc18939f3b904ec372cb2bee26fafdd501
Hunter Dixon <hdixon002@gmail.com>
2026-01-31 10:54:56 -0600

    Completed ex09 of ch05

--- chapter-5-loops/ex09.cpp
+++ chapter-5-loops/ex09.cpp
@@ -2,12 +2,29 @@
 // Created by hunter-dixon on 1/27/26.
 //
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
+// Financial application for computing future apartment rent
 int main()
 {
-    for (int i{}; i < 100; i++) {
-        std::cout << "hi\n";
+    // Initialize variables
+    double rent{1000};
+    double pct{0.03};
+    int numYears{5};
+
+    // Displays rent for the initial year
+    cout << "Year 1: $" << fixed << setprecision(2) << rent << "/mo." << "\n";
+
+    // Displays rent for the next four years
+    for (int i = 1; i < numYears; i++) {
+        rent = rent * (1 + pct);
+        cout << "Year " << i + 1 << ": $" << rent << "/mo." << "\n";
     }
+
+    // Displays the total rent for the fifth year
+    cout << endl;
+    cout << "Year 5 total: $" << rent * 12 << endl;
 
     return 0;
 }
